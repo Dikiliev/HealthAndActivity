4bac44dfff8a6dd38affab0f93ab951da1a4a594
Dikiliev <76054541+Dikiliev@users.noreply.github.com>
2024-05-12 09:35:05 +0300

    for vercel

--- config/settings.py
+++ config/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # DATABASES = {}
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
